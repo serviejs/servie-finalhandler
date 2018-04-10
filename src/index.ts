38584b39434fab8afcdb1e72b81997d1d4dc5932
Blake Embrey <hello@blakeembrey.com>
2018-04-10 20:09:35 +0900

    Update to Servie 2.0

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'servie'
+import { Request, Response, createHeaders } from 'servie'
+import { createBody } from 'servie/dist/body/universal'
 
 export function finalhandler (req: Request) {
   return async function (): Promise<Response> {
-    return new Response({
-      status: 404,
-      headers: {
-        'X-Content-Type-Options': 'nosniff',
-        'Content-Security-Policy': "default-src 'self'",
-        'Content-Type': 'text/html; charset=utf-8'
-      },
-      body: req.method === 'HEAD' ? undefined : `Cannot ${req.method} ${req.url}`
+    const statusCode = 404
+    const isHead = req.method.toLowerCase() === 'head'
+    const body = createBody(isHead ? undefined : `Cannot ${req.method} ${req.url}`)
+
+    const headers = createHeaders({
+      'X-Content-Type-Options': 'nosniff',
+      'Content-Security-Policy': "default-src 'self'"
     })
+
+    return new Response({ statusCode, headers, body })
   }
 }
